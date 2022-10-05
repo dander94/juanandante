19d5461dd63938a510bbedc69fd3654f14e985a4
dander94 <dander94@users.noreply.github.com>
2022-10-05 21:57:30 +0200

    feat: add fonts to the project

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -3,7 +3,11 @@
 </script>
 
 <template>
+<h1 class="is-size-1 has-text-weight-bold">Peña Juan Andante</h1>
 
+<h2 class="is-size-3 has-text-weight-medium">Johnny Walker en inglés</h2>
+
+<p class="is-family-secondary">Todos gritan al unísono <b>Real Betis Balompié</b></p>
 </template>
 
 <style scoped>
--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,7 +1,7 @@
 import { createApp } from 'vue'
 
-import './bulma.scss'
 import './style.scss'
+import './bulma.scss'
 
 import App from './App.vue'
 

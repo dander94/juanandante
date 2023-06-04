5608613c79bf96cd2de8037d32ea6e9ad3bf136b
dander94 <dander94@users.noreply.github.com>
2023-06-04 11:38:59 +0200

    feat (wip): add manage matches components

---
+++ frontend/src/components/dashboard/matches/management/ManageMatchCard.vue
@@ -0,0 +1,36 @@
+<template>
+  <MatchCard :match="match">
+    <template #actions>
+      <div class="tw-text-center">
+        <q-btn
+            class="tw-w-full"
+            color="primary"
+            :to="{ name: 'dashboard:match-request', params: { id: match.id } }"
+        >
+          Ver solicitudes
+        </q-btn>
+        <q-btn
+            class="tw-w-full"
+            color="primary"
+            flat
+            :icon="fasPencil"
+            :to="{ name: 'dashboard:match-request', params: { id: match.id } }"
+        >
+          Editar
+        </q-btn>
+      </div>
+    </template>
+  </MatchCard>
+</template>
+
+<script setup lang="ts">
+import { fasPencil } from '@quasar/extras/fontawesome-v6';
+
+import MatchCard from 'src/components/dashboard/matches/MatchCard.vue';
+import { Match } from 'src/types/match';
+
+withDefaults(defineProps<{
+  match: Match
+}>(), {
+});
+</script>
---
+++ frontend/src/pages/dashboard/management/ManageMatches.vue
@@ -0,0 +1,29 @@
+<template>
+  <q-page>
+    <div class="tw-container tw-mx-auto">
+      <div class="tw-p-5">
+        <h2 class="text-primary tw-text-2xl tw-font-bold tw-mb-4">
+          Partidos abiertos para solicitud
+        </h2>
+        <div class="tw-grid md:tw-grid-cols-2 lg:tw-grid-cols-4 tw-gap-8">
+          <div
+              v-for="match in matchStore.availableMatches"
+              :key="match.id"
+          >
+            <ManageMatchCard
+                :match="match"
+            />
+          </div>
+        </div>
+      </div>
+    </div>
+  </q-page>
+</template>
+
+<script setup lang="ts">
+import { useMatchStore } from 'src/stores/store-match';
+
+import ManageMatchCard from 'src/components/dashboard/matches/management/ManageMatchCard.vue';
+
+const matchStore = useMatchStore();
+</script>
